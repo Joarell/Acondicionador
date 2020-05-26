278a2fb59357d27838e8298a3b39564d41ef83d5
Joarell <42722369+Joarell@users.noreply.github.com>
2020-05-26 01:32:20 -0300

    Add files via upload

---
+++ packing.py
@@ -0,0 +1,11 @@
+class CrateWork:
+    def __init__(self, large, depth, weight);      #Medidas necessárias para definção da obra e caixa
+    self.large = large                             #Comprimento
+    self.depth = depth                             #Largura
+    self.weight = weight                           #Altura
+
+    def works (self, code);
+    self.works = works
+
+    def crate (self, crate);
+    self.crate = crate
