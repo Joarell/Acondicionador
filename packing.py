da40cd255529c17f2c03aa669db52bcf8e5e8ecb
Joarell <josephjoarell@gmail.com>
2020-09-07 13:46:14 -0300

    Modulo cirado - construtor e cubagem - ok

--- packing.py
+++ packing.py
@@ -1,11 +1,69 @@
-class CrateWork:
-    def __init__(self, large, depth, weight);      #Medidas necessárias para definção da obra e caixa
-    self.large = large                             #Comprimento
-    self.depth = depth                             #Largura
-    self.weight = weight                           #Altura
+class Pipedo:
+    cubing = float() 
 
-    def works (self, code);
-    self.works = works
+    def __init__(self, dimensions):      #Medidas necessárias para definção da obra e caixa
+        self.dimensions = dimensions
+       # sizes = dimensions
+        #print(sizes)
 
-    def crate (self, crate);
-    self.crate = crate
+    def cubing (self):
+        dimensions = self.dimensions
+        cubed = float()
+        total = float()
+        i = 0
+
+        for sizes in range(len(dimensions)):
+            cubing_calc = self.dimensions[i][0] * self.dimensions[i][1] * self.dimensions[i][2]/6000
+            i += 1
+            total += cubing_calc
+
+        cubed = total
+
+
+        return cubed
+
+#    def crate_factory(self):
+
+
+caixa = Pipedo([[100, 5, 90], [210, 10, 170], [60, 5, 90]])
+print("A cubagem total é de:" + '{:05.3f}' .format(caixa.cubing()) + "kgs cubados.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
